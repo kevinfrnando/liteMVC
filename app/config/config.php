885356293471cc45005085cef2519f551b6b874c
kevin_vergara <kevin_vergara.rodriguez@yahoo.es>
2020-05-30 13:29:12 -0500

    init commit

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+    // DDBB CONFIG
+
+    define("DB_HOST","localhost");
+    define("DB_USER","root");
+    define("DB_PASS","");
+    define("DB_NAME","karikum");
+
+    // RUTA DE LA APP
+    define("_ROUTE_APP", dirname(dirname(__FILE__)) );
+    define("_ROUTE_URL", dirname(dirname(__FILE__)) );
+    define("_ROUTE_SITE", dirname(dirname(__FILE__)) );
+
